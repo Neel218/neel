ea6d5465ec75fcc2c233beb64633164e851832ac
Neel218 <58074667+Neel218@users.noreply.github.com>
2020-11-10 09:28:47 +0200

    Add files via upload

---
+++ الخطوة الثالثة.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int mane(){
+    
+    int x[10] ,y[8] ,avrage;
+for(i = 1;i < 11;i++)
+{
+  cout <<"data students";
+  cin >>x;
+  for (int y = 0; y < 7; y++)
+  {
+    cout <<"Enter Material";
+    cin >>y;
+    cout <<"Enter Avrage/n";
+    cin>>y;
+    avarge=(sum y)/8;
+    cout>>avarge;
+
+  }
+  
+  
+
+}
+
+
+
+
+
+
+return 0;
+}
