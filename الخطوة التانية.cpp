ca9a8d63fda36ebbd8332da900c7ef51cf2b32c4
Neel218 <58074667+Neel218@users.noreply.github.com>
2020-11-10 09:28:14 +0200

    Add files via upload

---
+++ الخطوة التانية.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int mane(){
+    int a,;
+    char o,p,Z,X ;
+    cout<<o<<"Name: ";  //اخرج أسم الطالب
+    cin>>o;             //ادخل أسم الطالب
+    cout<<i<<"number: /n"; //يخرج رقم الطالب
+    cin>>i;               //أدخل رقم الطالب
+    cout<<p<<"Department: comuter /n"; //يخرج قسم الطالب
+    cin>>p;                 
+
+    int g=0; //عرف بيانات انها عدد حقيقي
+for(i = 1;i < 6;i++)
+{
+   cout <<" Enter G \n"; //  يطلب منك إدخال البيانات
+    cin>>g;              //ادخل  بيانات
+}
+
+
+
+
+
+
+return 0;
+}
